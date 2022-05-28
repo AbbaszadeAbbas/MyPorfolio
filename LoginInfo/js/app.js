9209a7cc6d49000b7d2a0e221f67dbb97145c03c
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-28 22:45:37 +0400

    LoginInfo

---
+++ LoginInfo/js/app.js
@@ -0,0 +1,14 @@
+let insideBox = document.querySelector(".com");
+
+function Yuxari() {
+  insideBox.style.transform = "translateY(580px)"
+
+}
+
+function Asagi() {
+  insideBox.style.transform = "translateY(-600px)"
+}
+
+function Sifirla() {
+  insideBox.style.transform = "translateY(0px)"
+}
